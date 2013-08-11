e8828c9c58745a3f1eb9e57c873e26f4fd693de9
Matt <dilzio@serenity.local>
2013-08-11 14:02:49 +0800

    cleanup

---
+++ build.sh
@@ -0,0 +1,5 @@
+erlc -o /Users/dilzio/Projects/erlcache/ebin /Users/dilzio/Projects/erlcache/src/*.erl
+cp /Users/dilzio/Projects/erlcache/ebin/res/simple_cache.app /Users/dilzio/Projects/erlcache/simple_cache/ebin 
+cp /Users/dilzio/Projects/erlcache/ebin/sc*.beam /Users/dilzio/Projects/erlcache/simple_cache/ebin 
+cp /Users/dilzio/Projects/erlcache/ebin/simple_cache.beam /Users/dilzio/Projects/erlcache/simple_cache/ebin 
+cp /Users/dilzio/Projects/erlcache/ebin/rd*.beam /Users/dilzio/Projects/erlcache/resource_diebin 
