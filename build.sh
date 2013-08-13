ec3e9722d1771cd82748a396fe56ca56de5d3b32
Matt <dilzio@serenity.local>
2013-08-13 17:29:08 +0800

    added release config

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 OUTDIR=./out/lib 
 SC_OUT_DIR=$OUTDIR/simple_cache/ebin 
 RD_OUT_DIR=$OUTDIR/resource_discovery/ebin 
-
+#clean
 rm -rf $OUTDIR
 mkdir -p $SC_OUT_DIR
 mkdir -p $RD_OUT_DIR
 
-
+#compile
 erlc +debug_info -o $SC_OUT_DIR ./src/simple_cache/*.erl
 cp ./res/simple_cache.app $SC_OUT_DIR
 
 erlc +debug_info -o $RD_OUT_DIR ./src/resource_discovery/*.erl
 cp ./res/resource_discovery.app $RD_OUT_DIR
 
+#dialyze
 dialyzer -I ./out/lib/simple_cache/ebin/*.beam ./out/lib/resource_discovery/ebin
+
+#generate boot scripts
+erl -detached -pa ./res -eval 'systools:make_script("simple_cache", [local, {outdir, "./out/lib"}]), init:stop().'
--- res/resource_discovery.app
+++ res/resource_discovery.app
@@ -1,8 +1,8 @@
 %% Copyright
 {application, resource_discovery, [
-  {description, "A Resource Disovery Application"},
+  {description, "A Resource Discovery Application"},
   {vsn, "1"},
-  {modules, [rd_app, rd_sup]},
+  {modules, [rd_app, rd_sup, resource_discovery]},
   {registered, [rd_sup]},
   {applications, [
     kernel,
