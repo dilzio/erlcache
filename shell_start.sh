07ec2bf4c2df9bdce827640009e2208f98f0c1dd
Matt <dilzio@serenity.local>
2013-08-21 16:20:25 +0800

    fixed bug

--- shell_start.sh
+++ shell_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-erl +W w -pa ./out/lib/simple_cache/ebin/ -pa ./out/lib/resource_discovery/ebin/ -pa ./out/lib/tcp_interface/ebin/ -pa ./out/lib http_interface/ebin -pa ./out/lib/gen_web_server/ebin -sname cache@localhost
+erl +W w -pa ./out/lib/simple_cache/ebin/ -pa ./out/lib/resource_discovery/ebin/ -pa ./out/lib/tcp_interface/ebin/ -pa ./out/lib/http_interface/ebin -pa ./out/lib/gen_web_server/ebin -sname cache@localhost
